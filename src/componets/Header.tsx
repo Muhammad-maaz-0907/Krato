21553605c5c3848d3484199c34fa4fd06b99af31
Muhammad-maaz-0907 <rajammaaz2006@gmail.com>
2025-04-30 13:38:38 +0500

    changes

--- src/componets/Header.tsx
+++ src/componets/Header.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import image2 from '../../public/image 2.jpg'
 
 const Header = () => {
   return (
     <header className='header'>
     <div>
       <Image
-      src="/krato-logo-2.png"
+      // src="/krato-logo-2.png"
+      src={image2}
       alt="logo"
       width={100}
       height={100}
@@ -23,11 +25,12 @@ const Header = () => {
            </ul> */}
 
 <ul>
-          <Link  href="/home">  <li> Home </li> </Link>
-          <Link  href="/about/page.tsx">  <li>About</li> </Link>
-          <Link  href="/packages"> <li> Packages </li> </Link>
-          <Link  href="/contact"> <li>Contact  </li> </Link>
-           </ul>
+  <li><Link href="/">Home</Link></li>
+    <li><Link href="/about">About</Link></li>
+    <li><Link href="/contact">Contact</Link></li>
+    <li><Link href="/packages">Packages</Link></li>
+          
+ </ul>
            
       </nav>
     </header>
